8056c59147292fa42cd5f7f2a392601aa72d0224
Erin Pierce <erinpierce13@gmail.com>
2015-09-10 22:06:10 -0400

    Added loop as per pg 35

--- Sprint1/Erin/cards.c
+++ Sprint1/Erin/cards.c
@@ -3,29 +3,36 @@
 int main()
 {
 	char card_name[3];
-	puts("Enter the card name:");
-	scanf("%2s",card_name);
-	int val =0;
-	if (card_name[0]=='K') {
-		val=10;
-	} else if (card_name[0]=='Q') {
-		val=10;
-	} else if (card_name[0]=='J') {
-		val=10;
-	} else if (card_name[0]=='A') {
-		val=11;
-	} else {
-		val=atoi(card_name);
+	int count =0;
+	while (val) {
+		puts("Enter the card name:");
+		scanf("%2s",card_name);
+		int val =0;
+		switch(card_name[0]) {
+			case 'K':
+			case 'Q':
+			case 'J':
+				val= 10;
+				break;
+			case 'A':
+				val=11;
+				break;
+			case 'X':
+				val=0;
+				break;
+			default:
+				val=atoi(card_name);
+				if ((val <0) || val >10) {
+					puts("Number cards are between 1 and 10");
+					continue
+				}
+		}	
+		if ((val>=3) && (val<=6)) {
+			count++;
+		} else if (val==10) {
+			count--;
+		}
+		printf("Current count: %i\n", count )
 	}
-printf("The card value is %i\n", val);
-
-/* Check if the value is 3 to 6 */
-if ((val>=3) && (val<=6)) {
-	puts("Count has gone up");
-}
-/* Otherwise check if the card was 10, J, Q, or K */ 
-else if (val==10) {
-	puts("Count has gone down");
-}
  return 0;
 }
