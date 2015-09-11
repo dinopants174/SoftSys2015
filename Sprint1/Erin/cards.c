65bd5c96a6d042ff04d111cca79a5cfb7e20f05d
Erin Pierce <erinpierce13@gmail.com>
2015-09-11 10:01:01 -0400

    Compiles without errors

--- Sprint1/Erin/cards.c
+++ Sprint1/Erin/cards.c
@@ -4,10 +4,10 @@ int main()
 {
 	char card_name[3];
 	int count =0;
+	int val =0;
 	while (val) {
 		puts("Enter the card name:");
 		scanf("%2s",card_name);
-		int val =0;
 		switch(card_name[0]) {
 			case 'K':
 			case 'Q':
@@ -24,7 +24,7 @@ int main()
 				val=atoi(card_name);
 				if ((val <0) || val >10) {
 					puts("Number cards are between 1 and 10");
-					continue
+					continue;
 				}
 		}	
 		if ((val>=3) && (val<=6)) {
@@ -32,7 +32,7 @@ int main()
 		} else if (val==10) {
 			count--;
 		}
-		printf("Current count: %i\n", count )
+		printf("Current count: %i\n", count );
 	}
  return 0;
 }
