40f55f997002ddcbafe694d8d795dfd9c13cf0ae
Erin Pierce <erinpierce13@gmail.com>
2015-09-10 20:57:02 -0400

    Compiles without errors

--- Sprint1/Erin/cards.c
+++ Sprint1/Erin/cards.c
@@ -1,10 +1,10 @@
-#include <sdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 int main()
 {
-	car card_name[3];
+	char card_name[3];
 	puts("Enter the card name:");
-	scanf("%2",card_name);
+	scanf("%2s",card_name);
 	int val =0;
 	if (card_name[0]=='K') {
 		val=10;
@@ -15,9 +15,9 @@ int main()
 	} else if (card_name[0]=='A') {
 		val=11;
 	} else {
-		val=atoi(card_name)
+		val=atoi(card_name);
 	}
 printf("The card value is %i\n", val);
 
-return 0
+return 0;
 }
