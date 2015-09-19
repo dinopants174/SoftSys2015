dd3eff75b89ed92123b0908c669532faa33d12fc
Erin Pierce <erinpierce13@gmail.com>
2015-09-19 19:28:50 -0400

    Got rid of the buffer bug!

--- Sprint1/Erin/cards_rf.c
+++ Sprint1/Erin/cards_rf.c
@@ -8,9 +8,13 @@ card names, and then counts how hot the deck is. */
 char* get_card() {
 //Asks the user to input a card, but returns
 //only the first two characters
+		int c;
 		static char card_name[3];
 		puts("Enter the card name:");
-		fgets(card_name,3,stdin);
+		// fgets(card_name,3,stdin);
+		scanf("%2s",card_name);
+		while((c = getchar()) != '\n' && c != EOF) //Flush the sdtin buffer
+		/* discard */ ;
 		printf("Current card inside f: %s\n", card_name);
 		return card_name;
 }
@@ -26,7 +30,7 @@ int count_cards(val,count) {
 // value of the current card
 	if ((val>=3) && (val<=6)) {
 			count++;
-		} else if (val==10) {
+		} else if ((val==10) || (val==11)){
 			count--;
 		}
 	return count;
@@ -41,10 +45,11 @@ int name2val(char* card_name){
 			val= 10;
 			break;
 		case 'A':
-			val=10;
+			val=11;
+			break;
 		default:
 			val=atoi(card_name);
-			if ((val <1) || (val >10)) {
+			if ((val <1) || (val >11)) {
 				puts("Number cards are between 1 and 10");
 				puts("Face Cards are A, K, Q, J");
 				val=0;
