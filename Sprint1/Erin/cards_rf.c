011aabf9f37d3e4a568ccba0a0340ed3b0d0b4c8
Erin Pierce <erinpierce13@gmail.com>
2015-09-19 19:33:44 -0400

    Took out extra prints and fixed bug where 2 was not counted

--- Sprint1/Erin/cards_rf.c
+++ Sprint1/Erin/cards_rf.c
@@ -11,11 +11,9 @@ char* get_card() {
 		int c;
 		static char card_name[3];
 		puts("Enter the card name:");
-		// fgets(card_name,3,stdin);
 		scanf("%2s",card_name);
 		while((c = getchar()) != '\n' && c != EOF) //Flush the sdtin buffer
 		/* discard */ ;
-		printf("Current card inside f: %s\n", card_name);
 		return card_name;
 }
 
@@ -28,7 +26,7 @@ char* init_card() {
 int count_cards(val,count) {
 // Updates the count of the deck based on the
 // value of the current card
-	if ((val>=3) && (val<=6)) {
+	if ((val>=2) && (val<=6)) {
 			count++;
 		} else if ((val==10) || (val==11)){
 			count--;
@@ -50,37 +48,30 @@ int name2val(char* card_name){
 		default:
 			val=atoi(card_name);
 			if ((val <1) || (val >11)) {
+				puts("Invalid Entry");
 				puts("Number cards are between 1 and 10");
 				puts("Face Cards are A, K, Q, J");
 				val=0;
 			}
 	}	
-	
 	return val;
 }
+
 int main()
 {
-	// char card_name[3];
 	int count=0;
 	int val;
 	char* card_name;
-	int loop=0;
 
 	card_name=init_card();
 
 	while (card_name[0]!='X') {
-		printf("Loop: %i\n", loop);
 		card_name=get_card();
-		printf("Current card: %s\n", card_name);
 		val=name2val(card_name);
-		printf("Current val: %i\n", val);
 		if (val){
 			count=count_cards(val,count);
 		}
 		printf("Current count: %i\n", count );
-		loop++;
-	}
-
-	
+	}	
  return 0;
 }
